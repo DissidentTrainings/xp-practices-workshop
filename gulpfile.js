e35778eb6ebf6befc6438bd8cd93b06238106b5a
Sebastian Schürmann <sebs@2xs.org>
2014-07-25 16:00:43 +0200

    Added a help function to the build

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,12 @@
 var gulp = require('gulp');
+require('gulp-help')(gulp);
+
 var markdown = require('gulp-markdown');
 
-gulp.task('default', function () {
-    return gulp.src('intro.md')
-        .pipe(markdown())
-        .pipe(gulp.dest('dist'));
+gulp.task('default', 'Builds all the stuff', function () {
+
 });
 
+gulp.task('createpractice', 'Creates a practice by passing a c2 wiki url', function () {
 
-
+});
